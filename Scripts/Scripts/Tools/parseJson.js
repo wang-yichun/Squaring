60d16d1ef3035e628024d9b1e506973e0b66f7d9
wang-yichun <wang-yichun@meyougames.com>
2015-01-11 01:53:23 +0800

    clean cache

--- Scripts/Scripts/Tools/parseJson.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by leadprogrammer on 8/18/14.
- */
-
-var parseJson = function (json_file) {
-    var text = cc.FileUtils.getInstance().getStringFromFile(json_file);
-    var json = JSON.parse(text);
-    return json;
-};
--- proj.android/proj.standard/obj/local/armeabi/objs/cocos2dx_store_static/__/__/Soomla/data/SoomlaJSONConsts.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_store_static/__/__/Soomla/data/SoomlaJSONConsts.o: \
- /Users/leadprogrammer/Documents/Squaring/proj.android/proj.standard/../../_PomeloLibrary/GameFoundation/extensions/cocos2dx-store/android/jni/../../Soomla/data/SoomlaJSONConsts.cpp \
- /Users/leadprogrammer/Documents/Squaring/proj.android/proj.standard/../../_PomeloLibrary/GameFoundation/extensions/cocos2dx-store/android/jni/../../Soomla/data/SoomlaJSONConsts.h
-
-/Users/leadprogrammer/Documents/Squaring/proj.android/proj.standard/../../_PomeloLibrary/GameFoundation/extensions/cocos2dx-store/android/jni/../../Soomla/data/SoomlaJSONConsts.h:
