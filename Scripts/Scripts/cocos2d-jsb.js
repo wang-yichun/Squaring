5092f7219d2e9cbd8995f1e0cc5fef9c7e01316b
wang-yichun <wang-yichun@meyougames.com>
2015-01-15 14:21:47 +0800

    Build 1.2

--- Scripts/Scripts/cocos2d-jsb.js
+++ Scripts/Scripts/cocos2d-jsb.js
@@ -98,10 +98,11 @@ cc.SpriteFrameCache.getInstance().addSpriteFrames(RES_TEX_PLIST_LayerCore);
 
 cc.ShaderCache.getInstance().loadDefaultShaders();
 
+Data.core = Core2;
 var para = {
     'scene_id': 'SceneA',  // 目标场景
     'groups': {             // 每个组的目标层
-        'core_group': 'LCore',
+        'core_group': 'LCore2',
         'popup_group': 'LMenuInGame'
     },
     'out_animation': {func_name: 'scene_out_animation_blackfade', para: [1]},
